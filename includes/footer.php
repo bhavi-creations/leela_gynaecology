8db937bebf523cfbc1376bbb3b2d8df232ad0b5d
manimalladi <manimalladi05@gmail.com>
2025-07-02 10:44:51 +0530

    changes

--- includes/footer.php
+++ includes/footer.php
@@ -41,6 +41,7 @@
             <!-- <li><a href="blogs.php" class=" text-dark">Blog</a></li> -->
             <li><a href="contact.php" class=" text-dark">Contact</a></li>
             <li><a href="appointments.php" class=" text-dark">Appointment</a></li>
+            <li><a href="https://g.co/kgs/iJhxqvV" class=" text-dark">GMB</a></li>
           </ul>
         </div>
       </div>
